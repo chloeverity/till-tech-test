7cf0e8eab8f85761187fae50a013209799b81c8c
Chloe Verity <chloeverity@hotmail.co.uk>
2019-02-08 12:26:38 +0000

    passing test to add an order into a hash with key=product, value=quantity

--- spec/OrderSpec.js
+++ spec/OrderSpec.js
@@ -5,6 +5,10 @@ describe("Order", () => {
     order = new Order;
   })
   it("Should initialize with an empty array", () => {
-    expect(order.content.length).toEqual(0)
+    expect(order.items.length).toEqual(0)
+  })
+  it("can add an order with a specified quantity", () => {
+    order.add("Tea", 2)
+    expect(order.items.length).toEqual(1)
   })
 })
--- src/Order.js
+++ src/Order.js
@@ -1,3 +1,10 @@
-function Order() {
-  this.content = []
+function Order(obj) {
+  this.items = []
 }
+
+Order.prototype.add = function (product, quantity) {
+  this.items.push({
+    key: product,
+    value: quantity
+  })
+};
